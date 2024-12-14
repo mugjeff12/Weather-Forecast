602486653b97b66b42bf32599e0bee2503ee2c1f
Mugdho <mugdhojeferson@gmail.com>
2024-12-14 22:04:45 +0600

    Final Commit

--- main.py
+++ main.py
@@ -4,21 +4,34 @@ from backend import get_data
 
 st.title("Weather forecast for the next days")
 place=st.text_input("Place")
+#place="Dhaka"
 days=st.slider("Forecast Days",min_value=1,max_value=5
                ,help="Select the number of days you want to forecast")
 option = st.selectbox("Select data to view",("Temperature","Sky"))
 
-st.subheader(f"{option} for the next {days} days in {place}")
 
-#dates=["2022-09-09","2022-09-10","2022-09-11"]
-#temperatures =[9,10,11]
 
-filtered_data = get_data(place,days,option)
+try:
+    if place:
 
-if option == "Temperature":
-    filtered_data = [x['main']['temp'] for x in filtered_data]
-    figure = px.line(x=[i for i in range(days)],y=filtered_data,labels={"x":"Date","y":"Temperature"})
-    st.plotly_chart(figure)
+        filtered_data = get_data(place,days)
+        st.subheader(f"{option} for the next {days} days in {place}")
+        if option == "Temperature":
+            temperatures = [dict['main']['temp'] for dict in filtered_data]
+            temperatures = [float(temp)/10 for temp in temperatures]
+            dates = [dict['dt_txt'] for dict in filtered_data]
+            m,n=len(temperatures),len(dates)
+            figure = px.line(x=dates,y=temperatures,labels={"x":"Date","y":"Temperature"})
+            st.plotly_chart(figure)
 
-if option == "Sky":
-    filtered_data = [x['weather'][0]['main'] for x in filtered_data]
+        if option == "Sky":
+            sky_conditions = [x['weather'][0]['main'] for x in filtered_data]
+            images = {"Clear":"images/clear.png", "Clouds":"images/cloud.png","Rain":"images/rain.png",
+                      "Snow":"images/snow.png"}
+            image_paths = [images[condition] for condition in sky_conditions]
+            date_desc = [dict['dt_txt'] for dict in filtered_data]
+            for img,date in zip(image_paths,date_desc):
+                st.image(img, width=115)
+                st.write(date)
+except (KeyError):
+    st.info("Please enter a valid place.")
